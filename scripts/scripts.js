6200c633c8b22e21d1e7edd27872560263c0d3ee
James Robiony-Rogers <43721451+JamisBuggerlugs@users.noreply.github.com>
2020-09-12 00:39:56 +1200

    11th September - Setting links using JS
    HTML:
    - Template page created 
    - Lists Page started 
    - Added classes for all links in order for JS to work 
    
    CSS:
    - Media query for tutorial cards when window less than 1420px 
      - So that the increased scale on hover works properly 
    
    JS:
    - Created var for all links 
    - Created var for all imgs 
    - Set links using JQuery class selector 
    - Set imgs using JQuery class selector
    
    NOTES: 
    - Now if I need to change a link, I will only need to change it in the 
    JS file once rather than finding each instance in all of the code on 
    every page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,31 @@
 
+// Page link vairable
+var homePage = "index.html"
+var loopsPage = "loops.html"
+var tryExceptPage = "try-except.html"
+var listsPage = "lists.html"
+var functionsPage = "functions.html"
+var classesPage = "classes.html"
+
+// Tutorial card vairables
+var loopsImg = "imgs/loops-card-img.png"
+var tryExceptImg = "imgs/try-except-card-img.png"
+var listsImg = "imgs/loops-card-img.png"          // var listsImg = "imgs/"
+var functionsImg = "imgs/loops-card-img.png"      // var functionsImg = "imgs/"
+var classesImg = "imgs/loops-card-img.png"        // var classesImg = "imgs/"
+
+
+// JQuery setting page links
+$(".home-page").attr("href", homePage);
+$(".loops-page").attr("href", loopsPage);
+$(".try-except-page").attr("href", tryExceptPage);
+$(".lists-page").attr("href", listsPage);
+$(".functions-page").attr("href", functionsPage);
+$(".classes-page").attr("href", classesPage);
+
+// JQuery setting tutorial card images
+$(".loops-page img").attr("src", loopsImg);
+$(".try-except-page img").attr("src", tryExceptImg);
+$(".lists-page img").attr("src", listsImg);
+$(".functions-page img").attr("src", functionsImg);
+$(".classes-page img").attr("src", classesImg);
