fbb3f20067c9f79a8b75f2582ffcbe35245bdc2d
James Robiony-Rogers <43721451+JamisBuggerlugs@users.noreply.github.com>
2020-09-17 22:28:24 +1200

    17th September - Finished Dropdown Bar
    HTML:
    - Copied dropdown bar accross to all current pages 
    
    CSS:
    - Fixed media query (max-width: 1420px)
    - Semi plannaed media query (max-width: 1100px)
    - Footer still doesnt stick to the bottom of the page 
    
    JS:
    - Dropdown on click 
    - Slide up dropdown menu when clicking anywhere but header/dropdown div 
    
    NOTES:
    - Still need to make the rest of the the tutorial card imgs (lists, 
    functions, classes)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,18 @@
 
 // Page link vairable
-var homePage = "index.html"
-var loopsPage = "loops.html"
-var tryExceptPage = "try-except.html"
-var listsPage = "lists.html"
-var functionsPage = "functions.html"
-var classesPage = "classes.html"
+var homePage = "index.html";
+var loopsPage = "loops.html";
+var tryExceptPage = "try-except.html";
+var listsPage = "lists.html";
+var functionsPage = "functions.html";
+var classesPage = "classes.html";
 
 // Tutorial card vairables
-var loopsImg = "imgs/loops-card-img.png"
-var tryExceptImg = "imgs/try-except-card-img.png"
-var listsImg = "imgs/loops-card-img.png"          // var listsImg = "imgs/"
-var functionsImg = "imgs/loops-card-img.png"      // var functionsImg = "imgs/"
-var classesImg = "imgs/loops-card-img.png"        // var classesImg = "imgs/"
+var loopsImg = "imgs/loops-card-img.png";
+var tryExceptImg = "imgs/try-except-card-img.png";
+var listsImg = "imgs/loops-card-img.png";          // var listsImg = "imgs/"
+var functionsImg = "imgs/loops-card-img.png";      // var functionsImg = "imgs/"
+var classesImg = "imgs/loops-card-img.png";        // var classesImg = "imgs/"
 
 
 // JQuery setting page links
@@ -35,20 +35,16 @@ $(".classes-page img").attr("src", classesImg);
 $(document).ready(function(){
 
    // JQuery Nav Dropdown
-   $(".dropdown-content").hide()  // hides the dropdown from the benginning
+   $('.dropdown-content').hide();  // hides the dropdown from the benginning
 
-   $('.dropdown').hover(function () {
-      $('.dropdown-content').slideDown();
-      // $('header').css('padding-bottom', "0px");
-      // $('.dropdown-content').css('padding-top', '20px');
+   // toggles between sliding down/up accordingly when clicking "Tutorials"
+   $('.dropdown').click(function () {
+      $('.dropdown-content').slideToggle();  // did have at just .slideUp()
    });
 
-   $('.dropdown-content').hover(function () {
-      $('.dropdown-content').slideDown();
-   }, function (){
+   // slides up the dropdown if clicking outside of the header/dropdown div
+   $('#title-container, .container, .tutorial-cards, .tutorial-title-container, .tutorial-container, footer').click(function () {
       $('.dropdown-content').slideUp();
-      // $('header').css('padding-bottom', "20px");
    });
-
 
 });
